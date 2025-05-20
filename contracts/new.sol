16116874c065a1df03bc83d0eb2a446d881fbd36
prakharkumar2515 <prakharkumar2515@gmail.com>
2025-05-20 18:14:22 +0530

    iinheritence and creating new contract inside contract

--- contracts/new.sol
+++ contracts/new.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-contract NewContract {
-    string secret;
+contract Ownable {
     address public owner;
 
     modifier onlyOwner() {
@@ -10,12 +9,34 @@ contract NewContract {
         _;
     }
 
-    constructor(string memory _secret) {
-        secret = _secret;
+    constructor() {
         owner = msg.sender;
     }
 
-    function getSecret() public view onlyOwner returns (string memory) {
+}
+
+contract SecretVault {
+    string secret;
+
+    constructor(string memory _secret) {
+        secret = _secret;
+    }
+
+    function getSecretOld() public view returns (string memory) {
         return secret;
     }
+}
+
+contract NewContract is Ownable { // Inheritence
+    address secretVault;
+
+    constructor(string memory _secret) {
+        SecretVault _secretVault = new SecretVault(_secret); // Creating new contract inside contract
+        secretVault = address(_secretVault);
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return SecretVault(secretVault).getSecretOld();
+    }
+       
 }
